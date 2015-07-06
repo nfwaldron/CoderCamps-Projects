4f8cd988b19203fb037b1bc3ade914c6d9227027
nfwaldron <waldron.nathan@gmail.com>
2015-07-06 15:08:55 -0700

    Enter a date as a string argument. Convert the string date to a DateTime date. Display date in long format.

---
+++ Convert2Date/Convert2Date/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert2Date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current date. The required format is mm/dd/yyyy: ");
+            
+            var date = Console.ReadLine();
+
+            var dateTime = DateTime.Parse(date);
+
+            Console.WriteLine( "The date is " + dateTime.ToString("D"));
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
