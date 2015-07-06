e430570e88a043ed1b4307333d74937821e856a5
nfwaldron <waldron.nathan@gmail.com>
2015-07-06 13:21:58 -0700

    C# Hello World

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Output information to the screen
+            Console.WriteLine("Hello World!");
+
+            //Receive input from the user
+            Console.ReadLine();
+        }
+    }
+}
