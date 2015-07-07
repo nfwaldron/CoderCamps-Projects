d958464ec3b23a0d89be94ff9165fca48ff01c5a
nfwaldron <waldron.nathan@gmail.com>
2015-07-07 13:21:41 -0700

    Create a calculator that adds a variable number of numbers. Utilize the parameter array to accomplish this.

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        
+        // Modify calculator so that it supports a variable number of aruguements.
+        // A parameter array enables you to accept zero to an infinite number of arguments. 
+        // The parameter array must be the last parameter listed as parameters that a method can accept. 
+        public double CalculateSum ( params double [] numberArray)
+        {
+            double sum = 0;
+
+            // Create a for loop to loop through the array
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                // Add the sum of the elements in the array
+                sum += numberArray[i];
+            }
+            // return the sum
+            return sum; 
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create new object of the Calculator class
+            var calculator = new Calculator();
+            // Call the CalculateSum method, and assign the value returned to the variable 'result'
+            var result = calculator.CalculateSum(3,3,3);
+            // Output the result
+            Console.WriteLine("Result is " + result);
+            // Pause the program
+            Console.ReadLine();
+
+        }
+    }
+}
