f1e7c153a6fca937e2c5e5e9bc6af1e0420f6030
nfwaldron <waldron.nathan@gmail.com>
2015-07-07 11:07:56 -0700

    Practice with Object Oriented Programming in C#

---
+++ OOP1/OOP1/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class Product
+    {
+        // To create a class property, type 'prop' tab, tab.
+        // Property is the price of the profuct
+        public decimal Price { get; set; }
+
+        // Name of the Product
+        public string Name { get; set; }
+
+        // Create a method to calculate tax and discounts (if applicable)
+        // A new feature of C# allows you to supply default values for paramenters. Syntax is 
+        // public decimal CalculateTax( decimal taxRate = 0.08m, decimal discount = 2.00m)
+        public decimal CalculateTax( decimal taxRate = 0.08m, decimal discount = 2.00m)
+        {
+            // The this keyword points to the 
+            return this.Price + (this.Price * taxRate - discount);
+        }
+
+    }
+}
---
+++ OOP1/OOP1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new instance of the class Product.
+            var product1 = new Product();
+            product1.Name = "Tesla";
+            product1.Price = 123000.00m;
+
+            // Another syntax for creating a new instance of an class Product.
+            // *** This is the recommended syntax, per Steve Walther ***
+            var eggs = new Product
+            {
+                Name = "Eggs",
+                Price = 3.50m
+            };
+
+            // display the name and tax
+
+            // Named arguements in C# allow you to label the arguements
+            var tax = eggs.CalculateTax(taxRate:0.08m, discount:0.0m);
+            Console.WriteLine("Tax is " + tax.ToString("c"));
+
+            // pause
+            Console.ReadLine();
+
+        }
+    }
+}
