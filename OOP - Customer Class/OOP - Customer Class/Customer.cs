6c5582d0341c9f0618deebba417729a18d47fce5
nfwaldron <waldron.nathan@gmail.com>
2015-07-07 11:07:22 -0700

    Practice with Object Oriented Programming in C#

---
+++ OOP - Customer Class/OOP - Customer Class/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP___Customer_Class
+{
+    class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        // Use input validation to ensure that the user cannot set an age that is less than zero for instances of the Customer class.
+        // Private variables are usually named _propertyName
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    _age = 0;
+                }
+                else
+                { _age = value; }
+            }
+        }
+        
+        public int BirthYear { get; set; }
+
+    }
+}
---
+++ OOP - Customer Class/OOP - Customer Class/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Create a customer class and add the following properties to the class. First Name, Last Name, Age, Birthdayte. Ensure that you are
+// using the right data types for each property.
+namespace OOP___Customer_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Local variables are in camelCase.
+            var sally = new Customer
+            {
+                FirstName = "Sally",
+                LastName = "Williams",
+                BirthYear = 2028,
+                Age = 23
+            };
+            Debug.Assert(sally.Age == 23, "Sally is 23");
+
+            var mike = new Customer
+            {   
+                FirstName = "Mike",
+                LastName = "Harrison",
+                Age = 10,
+                BirthYear = 2019
+            };
+
+            Debug.Assert(mike.Age == 10, "Mike is 10.");
+        }
+    }
+}
