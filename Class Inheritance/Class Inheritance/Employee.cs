5bd6308e7dd86a45d32b056ac17475c90cdcdee6
nfwaldron <waldron.nathan@gmail.com>
2015-07-07 16:48:51 -0700

    Inheritance Practice

---
+++ Class Inheritance/Class Inheritance/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Inheritance
+{
+    abstract class Employee
+    {
+        // Class Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Class Methods
+
+        public string ShowFullName()
+        {
+            Console.WriteLine("First Name: {0}, Last Name: {1}", FirstName, LastName);
+
+            return FirstName + " " + LastName ;     
+        }
+    }
+
+    class FullTimeEmployee: Employee
+    {
+        // Class Properties
+        public int YearsEmployed { get; set; }
+    }
+
+    class ContractEmployee: Employee
+    {
+        // Class Property
+        public int MonthsEmployed { get; set; }
+    }
+}
---
+++ Class Inheritance/Class Inheritance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bill = new FullTimeEmployee()
+            {
+                FirstName = "Bill",
+                LastName = "Gates",
+                YearsEmployed = 5
+            };
+
+            var steve = new ContractEmployee()
+            {
+                FirstName = "Steve",
+                LastName = "Jobs",
+                MonthsEmployed = 2
+            };
+
+            string result = bill.ShowFullName();
+            Console.WriteLine(result);
+            Console.ReadLine();
+            Debug.Assert(result == "Bill Gates");
+        }
+    }
+}
