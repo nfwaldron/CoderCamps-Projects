84414191061e3018d3989a1c203aa7d65c151efd
nfwaldron <waldron.nathan@gmail.com>
2015-07-07 09:33:58 -0700

    Convert a string containing the date to a DateTime Object, then return a formatted string.

--- Convert2Date/Convert2Date/Program.cs
+++ Convert2Date/Convert2Date/Program.cs
@@ -15,10 +15,13 @@ namespace Convert2Date
 
             Console.WriteLine("Please enter the current date. The required format is mm/dd/yyyy: ");
             
+            // The user input is stored as a string variable
             var date = Console.ReadLine();
 
+            // Use the .Parse() method of the DateTime object to string representation of date and time to its System.DateTime equivalent. 
             var dateTime = DateTime.Parse(date);
 
+            // Using the .ToString() method, change the variable dateTime to a string. The arguement ("D") is used as a format specifier.
             Console.WriteLine( "The date is " + dateTime.ToString("D"));
 
             Console.WriteLine("Enter a decimal: ");
