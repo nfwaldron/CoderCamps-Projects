2564a14bc74f34f52f6cd5f0af204347b706c078
nfwaldron <waldron.nathan@gmail.com>
2015-07-07 21:22:20 -0700

    Practice Concepts of Class Creation and Properties

---
+++ CC Day 2 Review/CC Day 2 Review/Customer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC_Day_2_Review
+{
+    class Customer
+    {
+        // Class Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthYear { get; set; }
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+
+            set {
+                if (value < 0)
+                {
+                    // Checks for a condition; if the condition is false, outputs messages and displays a message box that shows the call stack. 
+                    Debug.Assert(value > 0, "Age cannot be less than zero");
+
+                    // Assign the value to the private Class Property _age
+                    _age = 0;
+
+                    // Output the new age
+                    Console.WriteLine("Age is {0}", _age);
+
+                }
+
+                else
+	            {
+                    // If the value entered is greater than zero, assign the value in 'value' to _age = value
+                    _age = value;
+	            }
+            
+            }
+        }
+
+        // Class Methods
+
+        public void ShowData()
+        {
+            Console.WriteLine("First Name: {0}, Last Name: {1}, Age: {2}", this.FirstName, this.LastName, this.Age);
+        }
+        
+        
+    }
+}
---
+++ CC Day 2 Review/CC Day 2 Review/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC_Day_2_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mike = new Customer
+            {
+                FirstName = "Michael",
+                LastName = "Smith",
+                BirthYear = 1988,
+                Age = 50
+            };
+
+            var sandy = new Customer
+            {
+                FirstName = "Sandy",
+                LastName = "Smith",
+                BirthYear = 1989,
+                Age = 23
+            };
+            mike.ShowData();
+            sandy.ShowData();
+            Console.ReadLine();
+        }
+    }
+}
