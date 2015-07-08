2b25f5aed1260e8da2d6f5303cb97eaa934029f3
nfwaldron <waldron.nathan@gmail.com>
2015-07-08 15:36:42 -0700

    Generic List and Hashtable practice

---
+++ Generic List/Generic List/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_List
+{
+    class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movies = new List<Movie>
+            {
+                new Movie {Title="Star Wars"},
+                new Movie {Title="King Kong"},
+                new Movie {Title="Memento"}
+            };
+
+            foreach (var movie in movies)
+            {
+                Console.WriteLine(movie.Title);
+            }
+            
+
+        }
+    }
+}
---
+++ HashTable Collection - Unique Words/HashTable Collection - Unique Words/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Receive user input
+            var input = Console.ReadLine();
+            
+            // Split the string and return a string [] array
+            var words = input.Split(' ');
+
+            // Create a new hashtable
+            var hashTable = new Hashtable(); 
+
+            // Loop through each item in the string array 'words'
+            foreach (var item in words)
+            {
+             h              
+            }
+        }
+    }
+}
