eedbe78ab251ff78c7c1a70cb4d1bc9f64cf152d
nfwaldron <waldron.nathan@gmail.com>
2015-07-08 10:21:17 -0700

    Working with Class Libraries and Access Specifiers

---
+++ Creating a Class Library/Creating a Class Library/Program.cs
@@ -0,0 +1,22 @@
+﻿using MyUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creating_a_Class_Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var utility = new FormUtilities();
+
+            var cleanName = utility.CleanInput("  StEPheN WALthEr ");
+
+            Console.WriteLine(cleanName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Creating a Class Library/MyUtilities/FormUtilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUtilities
+{
+    internal class FormUtilities
+    {
+            public string CleanInput(string input)
+            {
+                return input.Trim().ToLowerInvariant();
+            }
+        
+    }
+}
