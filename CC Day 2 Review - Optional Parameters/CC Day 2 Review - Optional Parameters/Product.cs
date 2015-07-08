ed272e687edddb439feba9e51dc761a94da20fe9
nfwaldron <waldron.nathan@gmail.com>
2015-07-07 23:58:50 -0700

    Practice with Parameters

---
+++ CC Day 2 Review - Optional Parameters/CC Day 2 Review - Optional Parameters/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC_Day_2_Review___Optional_Parameters
+{
+    class Product
+    {
+        // Name
+        public string Name { get; set; }
+        // Price
+        public decimal Price { get; set; }
+        
+        public decimal CalculateTotal( decimal taxRate = 0, decimal discount = 0 )
+        {
+            return this.Price + (this.Price * taxRate) - (this.Price * discount);
+        }
+    }
+}
---
+++ CC Day 2 Review - Optional Parameters/CC Day 2 Review - Optional Parameters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC_Day_2_Review___Optional_Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product1 = new Product
+            {
+                Name = "Milk",
+                Price = 2.99m
+            };
+
+            // Price without tax and without a discount
+            var totalWithoutTax = product1.CalculateTotal();
+            Console.WriteLine("Product: {0}, Price: {1:c}", product1.Name, totalWithoutTax);
+
+            var totalWithTax = product1.CalculateTotal(taxRate:0.07m);
+            Console.WriteLine("Product: {0}, Price: {1:c}", product1.Name, totalWithTax);
+
+        }
+    }
+}
