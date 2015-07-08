b528ac0f08efb432a3a4f7df3d39e33b8a0b3e28
nfwaldron <waldron.nathan@gmail.com>
2015-07-08 11:25:51 -0700

    Create an extension method of the string [] that randomly selects an element in an array and outputs the result.

---
+++ ArrayExtension/ArrayExtension/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mars = "Martians Attack!";
+            var sea = "Seahawks Win Superbowl!";
+            var con = "Conner Destroys Hackathon Competition!";
+
+            var headlines = new string[] { mars, sea, con };
+
+            Console.WriteLine(headlines.GetRandom());
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
---
+++ ArrayExtension/ArrayExtension/StringExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExtension
+{
+    public static class StringExtension
+    {
+        public static string GetRandom(this string[] input)
+        {
+            // Create a new object of the random class
+            var random = new Random();
+            
+            // the Next method returns a non-negative random number that is less than the specified maximum
+            // the int returned is stored in the variable 'i'
+            var i = random.Next(input.Length);
+
+            // Output the result. The random number generated will also serve as the index of the element in the array.
+            return input[i];
+        }
+    }
+}
