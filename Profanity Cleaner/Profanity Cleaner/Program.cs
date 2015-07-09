8857973569b5d199c7ed0aaf69092df1b8a29dc2
nfwaldron <waldron.nathan@gmail.com>
2015-07-08 20:05:42 -0700

    Updated comments on profanity cleaner

--- Profanity Cleaner/Profanity Cleaner/Program.cs
+++ Profanity Cleaner/Profanity Cleaner/Program.cs
@@ -19,29 +19,34 @@ namespace Profanity_Cleaner
 
             var punctation = new char[' ', ',','!','.'];
 
-            // Split contents of the string 'input' wherever a space is encountered, and store in the string [] array words
-            //var words = input.Split();
+            // Split contents of the string 'input' wherever a punctuation is encountered, 
+            // and store in the string [] array words. This is accomplished using the Regex.Split() method. This method
+            // takes as arguements the string that is to be split, and the Regex code for the characters at which
+            // to split the string, before placing it into the array words.
             var words = Regex.Split(input, @"\b");
 
             // Create a new Dictionary of strings
-            // 
+            // So this is the way a Dictionary works...a Dictionary has keywords, or 'keys' and values/definitions
+            // When you create a dictionary, you enter as arguements the data type of the key, or the word, and the 
+            // data type of the definition, or the value.
             var profanityCleaner = new Dictionary<string, string>();
 
-            // Add your profane words to the dictionary
-            // So this is the way a Dictionary works...a Dictionary has keywords, or 'keys' and values/definitions
-            // 
-            // 
+            // Add your profane words to the dictionary.
+            // This is how the profanity cleaner works...we are going to add to the dictionary
+            // our swear words. These bad words will be the the key, and the word we want to change it to  
+            // will be the value.
             profanityCleaner.Add("darn", "d**n");
             profanityCleaner.Add("gosh", "g**h");
             profanityCleaner.Add("yuck", "y**k");
 
+            // Here we are going to create our dynamic collection List which is a list of string objects.
             var result = new List<string>();
 
             // For each item in the array words...
             foreach (var item in words)
             {
-                // As I go through each word in the list of words, if the word is in the profanity dictionary, 
-                // add the replacement word to result, else add the original word to result.
+                //...We are going to check to see if that item matches with a key in our dictionary.
+                // If the..............
                 if (profanityCleaner.ContainsKey(item))
                 {
                     result.Add(profanityCleaner[item]);
