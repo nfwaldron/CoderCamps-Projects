c14d6326d3dafb68ef0d886d26e80700a59a16c6
nfwaldron <waldron.nathan@gmail.com>
2015-07-09 13:43:33 -0700

    Exception handling

---
+++ ValidatingArguements/ValidatingArguements/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatingArguements
+{
+    class Product
+    {
+        private decimal _price;
+
+        public decimal Price
+        {
+            get { return _price; }
+
+            set
+            {
+                if (_price < 0.0m)
+                {
+                    throw new ArgumentOutOfRangeException("Price must be greater than zero");
+                }
+
+                _price = value; }
+        }
+        
+        public string Name { get; set; }
+
+        public decimal CalculateTotalPriceWithTax(decimal taxRate = 0.0m)
+        {
+            if (taxRate > 0.10m || taxRate < 0.0m)
+            {
+                throw new ArgumentOutOfRangeException("Value is outside of the acceptable range");
+            }
+
+                return this.Price + (this.Price * taxRate);
+            
+        }
+
+    }
+}
---
+++ ValidatingArguements/ValidatingArguements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatingArguements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product1 = new Product
+            {
+                Price = 10.00m,
+                Name = "Milk"
+            };
+            
+            // Within Range
+            var result = product1.CalculateTotalPriceWithTax(.07m);
+
+            // Out of Range High
+            // var result = product1.CalculateTotalPriceWithTax(.11m);
+
+            // Out of Range Low
+            //var result = product1.CalculateTotalPriceWithTax(-5.00m);
+
+            Console.WriteLine("The price of {0} = {1:c} ", product1.Name, result);
+            Console.Read();
+        }
+    }
+}
