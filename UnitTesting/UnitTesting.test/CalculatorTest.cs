996780b605224201e871cebe18370d8a2c54f8bc
nfwaldron <waldron.nathan@gmail.com>
2015-07-09 19:03:33 -0700

    Practice with Unit Testing

---
+++ UnitTesting/UnitTesting.test/CalculatorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting.test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        // There is a standard way to run unit tests. The three A's of unit testing.
+        // Arrange
+        // Here we do any setuup
+        // Act
+        // Assert
+        public void TestAddNumbers()
+        {
+            //Arrange
+            var calc = new Calculator();
+
+            //Act - run the method you want to test
+            var result = calc.AddNumbers(0,0);
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ UnitTesting/UnitTesting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting
+
+    // Unit tests are all about preventing errors and regression.
+    // In a place like Microsoft there are testers, and QA engineers. They create functional tests or exploratory testing.
+    // There are Automated Unit Testing and Functional Testing.
+    // Functional tests are from end to end testing.
+    // Unit tests are for testing a particular code block.
+
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
