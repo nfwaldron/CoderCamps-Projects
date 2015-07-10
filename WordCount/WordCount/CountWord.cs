3f359126adc33ad0d7db864d0b7fe8ac157c5373
nfwaldron <waldron.nathan@gmail.com>
2015-07-10 05:15:06 -0700

    Practice with word count

---
+++ WordCount/WordCount/CountWord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    class CountWord
+    {
+        public int WordCounter(string strLine)
+        {
+            // Split String into an Array.
+            var strSplit = strLine.Split(' ');
+            var strLength = strSplit.Length;
+            return strLength;
+        }
+    }
+}
---
+++ WordCount/WordCount/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wordCounter = new CountWord();
+            Console.WriteLine("Hello! Please enter a sentence: ");
+            var input = Console.ReadLine();
+
+            var wordNumber = wordCounter.WordCounter(input);
+            Console.WriteLine("Words in string = {0}", wordNumber);
+            Console.Read();
+        }
+    }
+}
