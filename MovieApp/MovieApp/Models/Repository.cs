08d744d50b574ff07998e02c97cc600a8f04e036
nfwaldron <waldron.nathan@gmail.com>
2015-07-12 00:07:22 -0700

    Movie Application

---
+++ MovieApp/MovieApp/Models/Repository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieApp.Models
+{
+    public class Repository
+    {
+        // 
+        public IList<Movie> ListMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Title = "Star Wars", Director = "George Lucas"},
+                new Movie { Title = "King Kong", Director = "Peter Jackson"},
+                new Movie { Title = "Momento", Director = "Christopher Nolan"}
+            };
+        }
+    }
+}
