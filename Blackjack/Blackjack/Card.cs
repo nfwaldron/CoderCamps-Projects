4fbbfe113b4faa4001d76617602fc1d1a9e6872d
nfwaldron <waldron.nathan@gmail.com>
2015-07-11 18:00:11 -0700

    Create a Blackjack game

---
+++ Blackjack/Blackjack/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    public class Card
+    {
+        // Card Class Properties 
+        public string ID { get; set; }
+        public string Suite { get; set; }
+        public int Value { get; set; }
+
+        public Card( string id, string suite, int value)
+        {
+            this.ID = id;
+            this.Suite = suite;
+            this.Value = value;
+        }
+
+
+    }
+}
---
+++ Blackjack/Blackjack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List of Cards
+            var hand = new List<Card>();
+            
+            // List of Cards
+            var deck = new List<Card>();
+            deck.Add(new card)
+        }
+    }
+}
