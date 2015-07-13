766bba1617f404e210f1b9d00a8dcfe3eecf7606
nfwaldron <waldron.nathan@gmail.com>
2015-07-13 16:24:47 -0700

    MVC Movie Application

---
+++ MovieApp2/MovieApp2/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieApp2.Controllers
+{
+    public class ProductsController : Controller // Controller is a class that inherits from base controller class.
+    {
+        // GET: Products
+        // 
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ MovieApp2/MovieApp2/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieApp2.Models
+{
+    public class Movie
+    {
+        public string MovieTitle { get; set; }
+        public string MovieDirector { get; set; }
+    }
+}
